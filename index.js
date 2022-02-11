3481cdf413430cd188dcf665eca94de6e78b37f2
R4Y815 <Akcent360@gmail.com>
2022-02-11 08:24:38 +0800

    initial ejs setup, in process

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 console.log('hello world');
+import express, {request, response } from 'express';
+import { read } from './jsonFileStorage.js';
+
+const app = express();
+const port = 3004;
+
+/* tells express how to serve static files and from 'public folder' */
+app.use(express.static('public'));
+// Set view engine
+app.set('view engine', 'ejs');
+
+app.get('/', (_, res) => {
+  read('data.json', (err, data) => {
+
+  response.render('home', data);
+  })
+});
+
+app.get('/', (req, res) => {
+  read('data.json', (err, data) => {
+
+  response.render('#', data);
+  })
+});
+
+app.get('/recipes/:index', (req, res) => {
+  read('data.json', (err, data) => {
+    const { index } = request.params;
+    const content = { index, recipe: data.recipes }
+  response.render('recipe', content);
+  })
+});
+
+
+
+app.listen(port,() => console.log('listening on Port:',port ));
